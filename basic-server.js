50f7dac4ccab356b520d4563a9d9cf44f09d119c
unknown <emon.me44@gmail.com>
2023-02-14 02:04:22 +0600

    second

--- basic-server.js
+++ basic-server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 
 // console.log(process.env.ACCESS_TOKEN_SECRET);
 
+// npm i cors mongodb dotenv express nodemon
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d02hkdv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 function run() {
